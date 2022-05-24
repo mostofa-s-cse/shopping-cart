4fffc765ab9ff10381a8f52c026db424e8fa0cfb
MOSTOFA_CSE <mostofa.s.cse@gmail.com>
2022-05-24 22:29:57 +0600

    shopping-cart all complete

--- js/main.js
+++ js/main.js
@@ -1,33 +1,71 @@
-function HandelPhoneChange(isIncrease) {
-    const phoneInput = document.getElementById("phone-count");
-    const phoneCount = parseInt(phoneInput.value)
-    let phoneNewCount = phoneCount;
+function handelProductChange(product, isIncrease) {
+    const productCount = getInputValue(product)
+    let productNewCount = productCount;
     if (isIncrease == true) {
-        phoneNewCount = phoneCount + 1;
+        productNewCount = productCount + 1;
     }
-    if (isIncrease == false && phoneCount > 0) {
-        phoneNewCount = phoneCount - 1;
+    if (isIncrease == false && productCount > 0) {
+        productNewCount = productCount - 1;
     }
-    phoneInput.value = phoneNewCount;
-    const phoneTotal = phoneNewCount * 1219;
-    document.getElementById("phone-pricse").innerText = '$' + phoneTotal;
+    document.getElementById(product + '-count').value = productNewCount;
+    let productTotal = 0;
+    if (product == 'phone') {
+        productTotal = productNewCount * 1219;
+    }
+    if (product == 'case') {
+        productTotal = productNewCount * 59;
+    }
+    document.getElementById(product + '-price').innerText = '$' + productTotal;
+    calculateTotal();
 }
 
+function calculateTotal() {
+    const phoneCount = getInputValue('phone');
+    const caseCount = getInputValue('case');
 
-function HandelProductChange(isIncrease) {
-    const caseInput = document.getElementById("case-count");
-    const caseCount = parseInt(caseInput.value)
-    let caseNewCount = caseCount;
-    if (isIncrease == true) {
-        caseNewCount = caseCount + 1;
-    }
-    if (isIncrease == false && caseCount > 0) {
-        caseNewCount = caseCount - 1;
-    }
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById("case-pricse").innerText = '$' + caseTotal;
+    const totalPrice = phoneCount * 1219 + caseCount * 59;
+    document.getElementById("total-price").innerText = '$' + totalPrice;
+    const tax = Math.round(totalPrice * 0.1);
+    document.getElementById("tax-amount").innerText = '$' + tax;
+    const grandTotal = totalPrice + tax;
+    document.getElementById("grand-total").innerText = '$' + grandTotal;
 }
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+}
+// function HandelPhoneChange(isIncrease) {
+//     const phoneInput = document.getElementById("phone-count");
+//     const phoneCount = parseInt(phoneInput.value)
+//     let phoneNewCount = phoneCount;
+//     if (isIncrease == true) {
+//         phoneNewCount = phoneCount + 1;
+//     }
+//     if (isIncrease == false && phoneCount > 0) {
+//         phoneNewCount = phoneCount - 1;
+//     }
+//     phoneInput.value = phoneNewCount;
+//     const phoneTotal = phoneNewCount * 1219;
+//     document.getElementById("phone-pricse").innerText = '$' + phoneTotal;
+// }
+
+
+// function HandelProductChange(isIncrease) {
+//     const caseInput = document.getElementById("case-count");
+//     const caseCount = parseInt(caseInput.value)
+//     let caseNewCount = caseCount;
+//     if (isIncrease == true) {
+//         caseNewCount = caseCount + 1;
+//     }
+//     if (isIncrease == false && caseCount > 0) {
+//         caseNewCount = caseCount - 1;
+//     }
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+//     document.getElementById("case-pricse").innerText = '$' + caseTotal;
+// }
 
 
 
